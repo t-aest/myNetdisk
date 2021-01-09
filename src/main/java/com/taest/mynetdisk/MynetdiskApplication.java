f71dc1c8162969eba6ab01f46f78282a938ff528
taest <876239615@qq.com>
2021-01-09 21:32:09 +0800

    项目的第一次创建

---
+++ src/main/java/com/taest/mynetdisk/MynetdiskApplication.java
@@ -0,0 +1,13 @@
+package com.taest.mynetdisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynetdiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynetdiskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taest/mynetdisk/MynetdiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taest.mynetdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynetdiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
