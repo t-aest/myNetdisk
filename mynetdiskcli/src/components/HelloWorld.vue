b977aae17ec9c83359cd8757484c7347250bf817
taest <876239615@qq.com>
2021-01-10 22:18:16 +0800

    前段ui的选用

--- mynetdiskcli/src/components/HelloWorld.vue
+++ mynetdiskcli/src/components/HelloWorld.vue
@@ -1,100 +1,186 @@
 <template>
-  <div class="layout">
-    <Menu mode="horizontal" theme="dark" active-name="1">
-      <div class="layout-logo"></div>
-      <div class="layout-nav">
-        <Menu-item name="1">
-          <Icon type="ios-navigate"></Icon>
-          导航一
-        </Menu-item>
-        <Menu-item name="2">
-          <Icon type="ios-keypad"></Icon>
-          导航二
-        </Menu-item>
-        <Menu-item name="3">
-          <Icon type="ios-analytics"></Icon>
-          导航三
-        </Menu-item>
-        <Menu-item name="4">
-          <Icon type="ios-paper"></Icon>
-          导航四
-        </Menu-item>
-      </div>
-    </Menu>
-    <Menu mode="horizontal" active-name="1">
-      <div class="layout-assistant">
-        <Menu-item name="1">二级导航</Menu-item>
-        <Menu-item name="2">二级导航</Menu-item>
-        <Menu-item name="3">二级导航</Menu-item>
-      </div>
-    </Menu>
-    <div class="layout-breadcrumb">
-      <Breadcrumb>
-        <Breadcrumb-item href="#">首页</Breadcrumb-item>
-        <Breadcrumb-item href="#">应用中心</Breadcrumb-item>
-        <Breadcrumb-item>某应用</Breadcrumb-item>
-      </Breadcrumb>
-    </div>
-    <div class="layout-content">
-      {{msg}}
-    </div>
-    <div class="layout-copy">
-      2011-2016 &copy; TalkingData
-    </div>
+  <div>
+    <el-container style="height: 100%" direction="vertical">
+      <el-header>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+          <el-menu-item disabled style="opacity:1" index="1">myNetdisk</el-menu-item>
+          <el-menu-item index="2">网盘</el-menu-item>
+          <el-menu-item index="3">消息中心</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-container>
+        <el-aside width="200px" style="text-align: center;border: solid 1px #e6e6e6">
+          <el-menu :default-openeds="['1', '3']" mode="vertical">
+            <el-submenu index="1">
+              <template slot="title"><i class="el-icon-message"></i>导航一</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="2-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-submenu index="3">
+              <template slot="title"><i class="el-icon-setting"></i>导航三</template>
+              <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="3-1">选项1</el-menu-item>
+                <el-menu-item index="3-2">选项2</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="分组2">
+                <el-menu-item index="3-3">选项3</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="3-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+          </el-menu>
+<!--          <el-tabs :tab-position="tabPosition">-->
+<!--            <el-tab-pane>-->
+<!--              <span slot="label"><i class="el-icon-folder" disabled="true"></i>&nbsp 全部文件</span>-->
+<!--            </el-tab-pane>-->
+<!--            <el-tab-pane label="图片"></el-tab-pane>-->
+<!--            <el-tab-pane label="文档"></el-tab-pane>-->
+<!--            <el-tab-pane label="视频"></el-tab-pane>-->
+<!--            <el-tab-pane label="种子"></el-tab-pane>-->
+<!--            <el-tab-pane label="音乐"></el-tab-pane>-->
+<!--            <el-tab-pane label="其它"></el-tab-pane>-->
+<!--            <el-tab-pane label="我的分享">-->
+<!--              <span slot="label"><i class="el-icon-share" disabled="true"></i>&nbsp 我的分享</span>-->
+<!--            </el-tab-pane>-->
+<!--            <el-tab-pane label="回收站">-->
+<!--              <span slot="label"><i class="el-icon-delete" disabled="true"></i>&nbsp 回收站</span>-->
+<!--            </el-tab-pane>-->
+<!--            <el-tab-pane label="回收站">-->
+<!--              <span slot="label"><el-progress :percentage="100" :format="format"></el-progress></span>-->
+<!--            </el-tab-pane>-->
+<!--          </el-tabs>-->
+        </el-aside>
+        <el-container>
+          <el-main>
+            <el-button size="small" type="primary">点击上传</el-button>
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+              <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+              <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+            </el-breadcrumb>
+            <el-table
+              ref="multipleTable"
+              :data="tableData"
+              tooltip-effect="dark"
+              style="width: 100%"
+              @selection-change="handleSelectionChange">
+              <el-table-column
+                type="selection"
+                width="55">
+              </el-table-column>
+              <el-table-column
+                label="日期"
+                width="120">
+                <template slot-scope="scope">{{ scope.row.date }}</template>
+              </el-table-column>
+              <el-table-column
+                prop="name"
+                label="姓名"
+                width="120">
+              </el-table-column>
+              <el-table-column
+                prop="address"
+                label="地址"
+                show-overflow-tooltip>
+              </el-table-column>
+            </el-table>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'display content'
+      msg: 'display content',
+      tabPosition: 'left',
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-08',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-06',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-07',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }]
     }
   }
 }
 </script>
-
 <style scoped>
-.layout{
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
 }
-.layout-logo{
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
+.el-menu{
+  border-right-style: none;
+  /*align-items: center; !* 水平居中 *!*/
+  /*justify-content: center; !* 垂直居中 *!*/
 }
-.layout-nav{
-  width: 420px;
-  margin: 0 auto;
-}
-.layout-assistant{
-  width: 300px;
-  margin: 0 auto;
-  height: inherit;
-}
-.layout-breadcrumb{
-  padding: 10px 15px 0;
-}
-.layout-content{
-  min-height: 200px;
-  margin: 15px;
-  overflow: hidden;
-  background: #fff;
-  border-radius: 4px;
-}
-.layout-content-main{
-  padding: 10px;
-}
-.layout-copy{
+.el-header, .el-footer {
+  color: #333;
   text-align: center;
-  padding: 10px 0 20px;
-  color: #9ea7b4;
+  line-height: 60px;
+}
+.el-aside {
+  color: #333;
+  float: left;
+  height: calc(100vh - 60px);
+}
+.el-main {
+  color: #333;
 }
 </style>
--- mynetdiskcli/src/main.js
+++ mynetdiskcli/src/main.js
@@ -3,10 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import iView from 'iview'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import 'iview/dist/styles/iview.css'
 Vue.config.productionTip = false
-Vue.use(iView)
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
