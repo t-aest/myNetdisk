234d902dc97ae0a3dedc8d24e83e7ce6f7d911b2
taest <876239615@qq.com>
2021-01-19 20:07:59 +0800

    修改jdk8之前的decoder为jdk8之后的

--- src/main/java/com/taest/mynetdisk/util/Base64ToMultipartFile.java
+++ src/main/java/com/taest/mynetdisk/util/Base64ToMultipartFile.java
@@ -1,9 +1,9 @@
 package com.taest.mynetdisk.util;
 
 import org.springframework.web.multipart.MultipartFile;
-import sun.misc.BASE64Decoder;
 
 import java.io.*;
+import java.util.Base64;
 
 public class Base64ToMultipartFile implements MultipartFile {
 
@@ -59,23 +59,15 @@ public class Base64ToMultipartFile implements MultipartFile {
     }
 
     public static MultipartFile base64ToMultipart(String base64) {
-        try {
-            String[] baseStrs = base64.split(",");
-
-            BASE64Decoder decoder = new BASE64Decoder();
-            byte[] b = new byte[0];
-            b = decoder.decodeBuffer(baseStrs[1]);
-
-            for(int i = 0; i < b.length; ++i) {
-                if (b[i] < 0) {
-                    b[i] += 256;
-                }
+        String[] baseStrs = base64.split(",");
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] b = new byte[0];
+        b = decoder.decode(baseStrs[1]);
+        for(int i = 0; i < b.length; ++i) {
+            if (b[i] < 0) {
+                b[i] += 256;
             }
-
-            return new com.example.demo.util.Base64ToMultipartFile(b, baseStrs[0]);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         }
+        return new Base64ToMultipartFile(b, baseStrs[0]);
     }
 }
