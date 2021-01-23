307498c18d52d14701a99d6c90845552c89dd18b
taest <876239615@qq.com>
2021-01-23 16:42:20 +0800

    文件类型图标收集，简单文件列表查询

--- mynetdiskcli/src/assets/js/util.js
+++ mynetdiskcli/src/assets/js/util.js
@@ -1,5 +1,58 @@
+export const FileTypes = {
+  PDFTYPE: 'application/pdf',
+  TEXTTYPE: 'text/plain',
+  CSSTYPE: 'text/css',
+  CSVTYPE: 'text/csv',
+  DOCTYPE: 'application/msword',
+  GIFTYPE: 'image/gif',
+  HTMLTYPE: 'text/html',
+  JPGTYPE: 'image/jpeg',
+  JSONTYPE: 'application/json',
+  MP3TYPE: 'audio/mpeg',
+  MP4TYPE: 'audio/mp4' || 'video/mp4',
+  PNGTYPE: 'image/png',
+  XLSTYPE: 'application/vnd.ms-excel',
+  XMLTYPE: 'application/xml' || 'text/xml',
+  ZIPTYPE: 'application/zip',
+  UNKNOWTYPE: 'application/octet-stream',
+  getIconByType(fileType) {
+    if (fileType === this.PDFTYPE) {
+      return 'static/img/pdf-icon.png'
+    } else if (fileType === this.TEXTTYPE) {
+      return 'static/img/txt-icon.png'
+    } else if (fileType === this.CSSTYPE) {
+      return 'static/img/css-icon.png'
+    } else if (fileType === this.CSSTYPE) {
+      return 'static/img/csv-icon.png'
+    } else if (fileType === this.DOCTYPE) {
+      return 'static/img/doc-icon.png'
+    } else if (fileType === this.GIFTYPE) {
+      return 'static/img/gif-icon.png'
+    } else if (fileType === this.HTMLTYPE) {
+      return 'static/img/html-icon.png'
+    } else if (fileType === this.JPGTYPE) {
+      return 'static/img/jpg-icon.png'
+    } else if (fileType === this.JSONTYPE) {
+      return 'static/img/json-icon.png'
+    } else if (fileType === this.MP3TYPE) {
+      return 'static/img/mp3-icon.png'
+    }else if (fileType === this.MP4TYPE) {
+      return 'static/img/mp4-icon.png'
+    }else if (fileType === this.PNGTYPE) {
+      return 'static/img/png-icon.png'
+    }else if (fileType === this.XLSTYPE) {
+      return 'static/img/txt-icon.png'
+    }else if (fileType === this.XMLTYPE) {
+      return 'static/img/xml-icon.png'
+    }else if (fileType === this.ZIPTYPE) {
+      return 'static/img/zip-icon.png'
+    }else {
+      return 'static/img/unknown-icon.png';
+    }
+  }
+}
 export const Util = {
-  formatdate :function (dateString,type){
+  formatdate: function (dateString, type) {
     let date = new Date(dateString)
     let year = date.getFullYear();
     let month = addzero(date.getMonth() + 1);
@@ -7,16 +60,25 @@ export const Util = {
     let hour = addzero(date.getHours());
     let minute = addzero(date.getMinutes());
     let second = addzero(date.getSeconds());
+
     function addzero(value) {
-      if(value<10){
-        value="0"+value;
+      if (value < 10) {
+        value = "0" + value;
       }
       return value
     }
-    if(type==1){
+
+    if (type == 1) {
       return (year + "-" + month + "-" + weekday);
-    }else{
-      return (year + "-" + month + "-" + weekday+" "+hour+":"+minute+":"+second);
+    } else {
+      return (year + "-" + month + "-" + weekday + " " + hour + ":" + minute + ":" + second);
     }
-  }
+  },
+  getIcon(fileType) {
+    if (fileType === 'text/plain') {
+      return 'static/img/image-icon.png'
+    } else {
+      return 'el-icon-view'
+    }
+  },
 }
--- src/main/java/com/taest/mynetdisk/file/controller/FileController.java
+++ src/main/java/com/taest/mynetdisk/file/controller/FileController.java
@@ -4,9 +4,15 @@ package com.taest.mynetdisk.file.controller;
 import com.taest.mynetdisk.dto.FileDto;
 import com.taest.mynetdisk.file.entity.MyFile;
 import com.taest.mynetdisk.file.service.IFileService;
+import com.taest.mynetdisk.response.BaseController;
 import com.taest.mynetdisk.response.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -19,22 +25,44 @@ import java.util.List;
  * @since 2021-01-11
  */
 @RestController
-@RequestMapping("/admin")
-public class FileController{
+@Api(tags = "文件管理")
+@RequestMapping("file")
+public class FileController extends BaseController {
 
     @Autowired
     private IFileService fileService;
 
+    @ApiOperation(value = "文件列表",notes = "文件列表查询")
     @GetMapping("/files")
     public Result list(){
-        Result result = new Result();
         List<MyFile> list = fileService.list();
-        result.setData(list);
-        return result;
+        return success(list);
     }
 
-    @PostMapping("/file")
-    public Result upload(FileDto fileDto){
+    @ApiOperation(value = "文件上传",notes = "文件上传")
+    @PostMapping("/upload")
+    public Object upload(
+            @RequestParam("relativePath") String path,
+            @RequestParam("filename") String name,
+            @RequestParam("parentId") String parentId,
+            @RequestParam("fileType") String fileType,
+            @RequestParam("totalSize") Integer fileSize,
+            @RequestParam("chunkNumber") Integer shardIndex,
+            @RequestParam("chunkSize") Integer shardSize,
+            @RequestParam("totalChunks") Integer shardTotal,
+            @RequestParam("identifier") String key,
+            @RequestParam("uploadFile") MultipartFile uploadFile){
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(name);
+        fileDto.setParentId(parentId);
+        fileDto.setFileType(fileType);
+        fileDto.setFileSize(fileSize);
+        fileDto.setShardIndex(shardIndex);
+        fileDto.setShardSize(shardSize);
+        fileDto.setShardTotal(shardTotal);
+        fileDto.setFileKey(key);
+        fileDto.setFile(uploadFile);
         return fileService.uploadFile(fileDto);
     }
 
