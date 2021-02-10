f5d489846b92dd84b2f802511a2ef67db3112a18
taest <876239615@qq.com>
2021-02-10 12:57:38 +0800

    improve simple Security

--- src/main/java/com/taest/mynetdisk/bussiness/user/service/MyUserDetailsService.java
+++ src/main/java/com/taest/mynetdisk/bussiness/user/service/MyUserDetailsService.java
@@ -1,5 +1,12 @@
 package com.taest.mynetdisk.bussiness.user.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.taest.mynetdisk.bussiness.user.entity.MyUser;
+import com.taest.mynetdisk.bussiness.user.mapper.MyUserMapper;
+import com.taest.mynetdisk.exception.file.UserException;
+import com.taest.mynetdisk.response.ResultStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -14,10 +21,21 @@ import java.util.List;
 
 @Service("userDetailsService")
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private MyUserMapper myUserMapper;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        //根据用户名查询用户信息
+        QueryWrapper<MyUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("login_name", userName);
+        MyUser myUser = myUserMapper.selectOne(queryWrapper);
+        if (StringUtils.checkValNull(myUser)){
+            throw new UserException(ResultStatus.USER_NOT_EXIST,null);
+        }
         List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("role");
 
-        return new User("twd", new BCryptPasswordEncoder().encode("123"), auths);
+        return new User(myUser.getLoginName(), new BCryptPasswordEncoder().encode(myUser.getPassword()), auths);
     }
 }
