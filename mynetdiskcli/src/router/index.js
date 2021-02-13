cdc23ed076fe6dc875262bba6223eb16301e253a
taest <876239615@qq.com>
2021-02-13 17:56:11 +0800

    modify router

--- mynetdiskcli/src/router/index.js
+++ mynetdiskcli/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
+import Login from '../views/Login'
 
 Vue.use(Router)
 
@@ -9,11 +10,22 @@ Router.prototype.push = function push (to) {
   return VueRouterPush.call(this, to).catch(err => err)
 }
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Main',
-      component: Main
-    }
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+    path: '*',
+    redirect: '/login'
+  }, {
+    path: '',
+    redirect: '/login'
+  }, {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/main',
+    name: 'Main',
+    component: Main
+  }
   ]
 })
