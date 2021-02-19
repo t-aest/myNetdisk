a22110d497279653addba3daf056f9ce18bc55be
taest <876239615@qq.com>
2021-02-19 21:13:38 +0800

    add LoginUserDto implements UserDetails

---
+++ src/main/java/com/taest/mynetdisk/dto/LoginUserDto.java
@@ -0,0 +1,63 @@
+package com.taest.mynetdisk.dto;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+public class LoginUserDto implements UserDetails {
+
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    private final String username;
+    private final String password;
+    private static final long serialVersionUID = 1L;
+    private UserDto user;
+
+    public LoginUserDto(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
