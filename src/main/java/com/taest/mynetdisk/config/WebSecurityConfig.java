fab852cc3559a0b213b19b8de4352b8fa87c4779
taest <876239615@qq.com>
2021-02-22 19:36:02 +0800

    disable csrf

--- src/main/java/com/taest/mynetdisk/config/WebSecurityConfig.java
+++ src/main/java/com/taest/mynetdisk/config/WebSecurityConfig.java
@@ -1,20 +1,32 @@
 package com.taest.mynetdisk.config;
 
+//import com.taest.mynetdisk.filter.AdminAuthenticationProcessingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+//    private final AdminAuthenticationProcessingFilter adminAuthenticationProcessingFilter;
+//
+//    public WebSecurityConfig(AdminAuthenticationProcessingFilter adminAuthenticationProcessingFilter) {
+//        this.adminAuthenticationProcessingFilter = adminAuthenticationProcessingFilter;
+//    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -23,11 +35,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.cors().and().csrf().disable();
+//        http.authorizeRequests()
+//                .antMatchers("/user/login/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and().httpBasic()
+//                .and().csrf().disable();
+//        .addFilterAt(adminAuthenticationProcessingFilter, UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/kaptcha/**")
+                .antMatchers("/user/login");
     }
 
     @Bean
     PasswordEncoder password(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
