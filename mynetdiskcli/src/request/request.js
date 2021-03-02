7709f10bc2063fef469e63214f220a53ca7a6b55
taest <876239615@qq.com>
2021-03-02 19:27:20 +0800

    add vue without parameters get request

--- mynetdiskcli/src/request/request.js
+++ mynetdiskcli/src/request/request.js
@@ -132,6 +132,22 @@ export function get (url, params) {
 }
 
 /**
+ * get方法，对应get请求
+ * @param {String} url [请求的url地址]
+ * @param {Object} params [请求时携带的参数]
+ */
+export function noParameGet (url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(res => {
+        resolve(res.data)
+      }).catch(err => {
+        reject(err.data)
+      })
+  })
+}
+
+/**
  * post方法，对应post请求
  * @param {String} url [请求的url地址]
  * @param {Object} params [请求时携带的参数]
