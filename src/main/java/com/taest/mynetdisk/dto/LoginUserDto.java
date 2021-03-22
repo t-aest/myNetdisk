28b0cc4c69f9f8033b63bd8ba4ebaa1d661aad98
taest <876239615@qq.com>
2021-03-22 19:12:07 +0800

    modify loginuser

--- src/main/java/com/taest/mynetdisk/dto/LoginUserDto.java
+++ src/main/java/com/taest/mynetdisk/dto/LoginUserDto.java
@@ -1,22 +1,20 @@
 package com.taest.mynetdisk.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
-
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 public class LoginUserDto implements UserDetails {
 
     private final Collection<? extends GrantedAuthority> authorities;
 
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
     private static final long serialVersionUID = 1L;
     private UserDto user;
 
